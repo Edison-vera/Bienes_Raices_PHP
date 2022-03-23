5cc26c3954e4ca3c8a2e54c982e027c5051bf4da
Edison-vera <edisonefvc15@gmail.com>
2022-03-22 19:53:32 -0500

    Hashear Passwords

--- usuario.php
+++ usuario.php
@@ -7,10 +7,16 @@ $db = conectarDB();
 //Crear un email y password
 $email = "correo@correo.com";
 $password = 123456;
-//Query para crear el usuario 
-$query = " INSERT INTO usuarios (email, password) VALUES ('${email}', '${password}');";
 
-echo $query;
+$passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+
+
+//Query para crear el usuario 
+$query = " INSERT INTO usuarios (email, password) VALUES ('${email}', '${passwordHash}');";
+
+// echo $query;
+
 
 
 //Agregarlo a la base de datos 
