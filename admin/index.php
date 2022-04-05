b0890e41952641a1308af32c9603d38d411a4e16
Edison-vera <edisonefvc15@gmail.com>
2022-04-04 20:08:51 -0500

    Iterando sobre los resultados

--- admin/index.php
+++ admin/index.php
@@ -6,7 +6,7 @@ estaAutenticado();
 use App\Propiedad;
 
 //Implementar un metodo para obtener todas las propiedades 
-$propiedad = Propiedad::all();
+$propiedades = Propiedad::all();
 
 //Muestra mensaje condicional
 $resultado =$_GET["resultado"] ?? null;
@@ -69,24 +69,24 @@ incluirTemplate("header");
               </thead>
             
               <tbody> <!-- Mostrar los resultados --->
-              <?php while($propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
+              <?php foreach($propiedades as $propiedad):  ?>
                     <tr>
-                        <td><?php echo $propiedad["id"]; ?></td>
-                        <td><?php echo $propiedad["titulo"]; ?></td>
-                        <td> <img src="../../BienesRaices/imagenes/<?php echo $propiedad["imagen"]; ?>" class="imagen-tabla"></td>
-                        <td>$ <?php echo $propiedad["precio"]; ?></td>
+                        <td><?php echo $propiedad->id; ?></td>
+                        <td><?php echo $propiedad->titulo; ?></td>
+                        <td> <img src="../../BienesRaices/imagenes/<?php echo $propiedad->imagen; ?>" class="imagen-tabla"></td>
+                        <td>$ <?php echo $propiedad->precio; ?></td>
                         <td>
                            <form method="POST" class="w-100">
 
-                                <input type="hidden" name="id" value="<?php echo $propiedad["id"];?>">
+                                <input type="hidden" name="id" value="<?php echo $propiedad->id;?>">
 
 
                                 <input type="submit"  class="boton-rojo-block" value="Eliminar">
                            </form>
-                            <a href="../../BienesRaices/admin/propiedades/actualizar.php?id=<?php echo $propiedad["id"]; ?>" class="boton-amarillo-block">Actualizar</a>    
+                            <a href="../../BienesRaices/admin/propiedades/actualizar.php?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">Actualizar</a>    
                         </td>
                     </tr>
-                    <?php endwhile; ?>
+                    <?php endforeach; ?>
               </tbody>
 
 
